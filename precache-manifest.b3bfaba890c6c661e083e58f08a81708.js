6aac80a8b2483cdbbe7f56a1c2cbbe15ac14e570
ravendano014 <ronald.avendano@gmail.com>
2020-09-17 21:09:40 -0600

    Updates

---
+++ precache-manifest.b3bfaba890c6c661e083e58f08a81708.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc2d9021ea8c4b10d31590462733f63d",
+    "url": "/react-edictus/index.html"
+  },
+  {
+    "revision": "bc792b61d4a060df15be",
+    "url": "/react-edictus/static/css/main.849fc741.chunk.css"
+  },
+  {
+    "revision": "c80f564a06d84bb98a89",
+    "url": "/react-edictus/static/js/2.c11ef73d.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/react-edictus/static/js/2.c11ef73d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc792b61d4a060df15be",
+    "url": "/react-edictus/static/js/main.3893c683.chunk.js"
+  },
+  {
+    "revision": "738e968943f71d3c168a",
+    "url": "/react-edictus/static/js/runtime-main.7c52c82a.js"
+  }
+]);
